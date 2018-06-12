8feec0d8b39c37870fae9e2eba385f2f2ab6a8a4
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-12 20:25:20 +0100

    can fly to chicago

--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -3,13 +3,14 @@ const MapWrapper = function(element, coords, zoom){
   this.map = L.map(element).addLayer(osmLayer).setView(coords, zoom);
   this.map.on('click', function(event){
     this.addMarker(event.latlng);
-    // const chicago = [41.8781, 87.6298];
-    // const button = document.querySelector('button');
-    // button.addEventListener('click', function(){
-    //   this.takeMeToChicago(chicago);
-  // });
+
   }.bind(this))
 
+  const chicago = [41.8781, -87.6298, 15];
+  const button = document.querySelector('button');
+  button.addEventListener('click', function(){
+    this.takeMeToChicago(chicago);
+}.bind(this));
 
 
 }
@@ -19,6 +20,6 @@ MapWrapper.prototype.addMarker = function(coords){
 
 };
 
-  // MapWrapper.prototype.takeMeToChicago = function (coords) {
-  //   L.panTo(coords).addTo(this.map);
-  // };
+  MapWrapper.prototype.takeMeToChicago = function (coords, zoom) {
+    this.map.flyTo(coords, zoom);
+  };
